0fd1bfbe9f85083f27cdcd8250bc7f0924643f84
Yazheviks <vicsergeyeva@gmail.com>
2020-10-21 16:19:10 +0300

    Updates

---
+++ precache-manifest.0e9f916eb19dd99d008fdb6b48e247cd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "47b95aeff7dfc51d361b3856a9f87277",
+    "url": "./index.html"
+  },
+  {
+    "revision": "2106be90d140d080e4ab",
+    "url": "./static/css/main.819e4dd6.chunk.css"
+  },
+  {
+    "revision": "9bfabb54970d8a1edebf",
+    "url": "./static/js/2.c17dec48.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.c17dec48.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2106be90d140d080e4ab",
+    "url": "./static/js/main.c6059e4b.chunk.js"
+  },
+  {
+    "revision": "89de80f3985b25002b5f",
+    "url": "./static/js/runtime-main.17b9eccd.js"
+  }
+]);
